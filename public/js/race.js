1b28545a87784acd2aad26d67a6b64b37151334a
meskyA <mesky2015@gmail.com>
2022-09-20 21:35:47 -0700

    initial commit

--- public/js/race.js
+++ public/js/race.js
@@ -1,23 +1,23 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#user-name').value.trim();
+  const userID = document.querySelector('#user-id').value.trim();
+  const message = document.querySelector('#message').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && userID && message) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, userID, message }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/race');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create race');
     }
   }
 };
@@ -31,17 +31,17 @@ const delButtonHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/race');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete race');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-race-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.race-list')
   .addEventListener('click', delButtonHandler);
