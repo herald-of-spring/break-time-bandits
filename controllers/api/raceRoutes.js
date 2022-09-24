ec2d03ac39e896e4d3de7d13054988fdb200b528
meskyA <mesky2015@gmail.com>
2022-09-24 15:33:02 -0700

    works on controller/api folder, changed route names

--- controllers/api/raceRoutes.js
+++ controllers/api/raceRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Race } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newRace = await Race.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newRace);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const raceData = await Race.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!raceData) {
+      res.status(404).json({ message: 'No race found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(raceData);
   } catch (err) {
     res.status(500).json(err);
   }
