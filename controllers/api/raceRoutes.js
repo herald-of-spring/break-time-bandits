913140e8bc2ab28597e742d2d6b2d362869f7e36
meskyA <mesky2015@gmail.com>
2022-09-27 14:51:35 -0700

    modifies raceRouter.js

--- controllers/api/raceRoutes.js
+++ controllers/api/raceRoutes.js
@@ -11,8 +11,8 @@ router.post('/create', withAuth, async (req, res) => {
       user_id: req.session.username,
       race_id: newRace.race_id
     })
-    res.render('race', {   ...race,
-      logged_in: req.session.logged_in
+    res.render('create', {   newRace, hostSelection,
+      user_id: req.session.username
   });
   } catch (err) {
     res.status(400).json(err);
