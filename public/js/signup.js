e8b96c578e23725da0c669ef28b3c7e0c8fe80c4
meskyA <mesky2015@gmail.com>
2022-09-28 14:35:49 -0700

    adds comments

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,9 @@
+// This is a formHandler to create a new race form in the homepage.handlebars.
 const loginFormHandler = async function(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
     console.log(username);
     console.log(password);
     const response = await fetch('/api/user', {
